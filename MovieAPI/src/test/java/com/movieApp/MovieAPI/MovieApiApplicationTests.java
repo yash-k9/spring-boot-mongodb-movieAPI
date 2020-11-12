506756603c946188f3137cb211d69b69190daacc
Kurella Yash <59912430+yash-k9@users.noreply.github.com>
2020-11-12 16:37:50 +0530

    Added MovieAPI

---
+++ MovieAPI/src/test/java/com/movieApp/MovieAPI/MovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieApp.MovieAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
